68d201c53d18d4238f7d2f15bd9150e0f69aae13
Christian Ramos <chrisramfon@outlook.com>
2022-06-30 17:38:05 -0500

    Creacion de conexion con mysql

---
+++ recetas-backend/controllers/mysqlconnection.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+require('dotenv').config();
+
+const mysqlconnection = {}
+mysqlconnection.conf() =  mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+module.exports = mysqlconnection;
