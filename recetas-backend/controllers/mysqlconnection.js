e16640f829c7813e037e25598139700d43f83d26
Christian Ramos <chrisramfon@outlook.com>
2022-07-02 12:18:22 -0500

    Busca al usuario en la base de datos

--- recetas-backend/controllers/mysqlconnection.js
+++ recetas-backend/controllers/mysqlconnection.js
@@ -2,11 +2,12 @@ var mysql = require('mysql');
 require('dotenv').config();
 
 const mysqlconnection = {}
-mysqlconnection.conf() =  mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+
+mysqlconnection.conf =  mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Instala1.",
+    database: "recetas"
 });
 
 module.exports = mysqlconnection;
--- recetas-backend/controllers/signin.js
+++ recetas-backend/controllers/signin.js
@@ -6,21 +6,25 @@ const register = {}
 
 register.reg = async(req, res)=>{
     try{
-        const pass = "Instala1.";
-        const salt = await bcrypt.genSalt(10);
-        const encrypted = await bcrypt.hash(pass, salt);
+        const usuario = req.body.Usuario;
+        const queryU = util.promisify(conn.conf.query).bind(conn.conf);
+        const rowsU = await queryU('select * from Usuario where Usuario like ?', [usuario]);
+        console.log(rowsU);
 
-        const queryE  = util.promisify(conn.conf.query).bind(conn.conf);
-        const rowsE = await queryE('insert into Usuario (Usuario, Pass, Status) values (?, ?, 1)', ['chrisramfon', encrypted]);
-        const respuesta = {Mensaje: "Usuario registrado con éxito."}
-        res.send(respuesta).status(200);
+        //const pass = "Instala1.";
+        //const salt = await bcrypt.genSalt(10);
+        //const encrypted = await bcrypt.hash(pass, salt);
+
+        //const queryE  = util.promisify(conn.conf.query).bind(conn.conf);
+        //const rowsE = await queryE('insert into Usuario (Usuario, Pass, Status) values (?, ?, 1)', ['chrisramfon', encrypted]);
+        //const respuesta = {Mensaje: "Usuario registrado con éxito.", Datos: rowsU}
+        res.send('bien').status(200);
     }catch(error){
         respuesta = {mensaje: "No se pudo registrar el usuario.", Error: error}
         res.send(respuesta).status(400);
     }
 }
 
-
-
+module.exports = register;
 
 
