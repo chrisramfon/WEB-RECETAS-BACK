d24fae2de7b28e6c4d09690e8d94e2c8bf073d46
Christian Ramos <chrisramfon@outlook.com>
2022-07-19 13:37:34 -0500

    Registro de receta con promesas

--- recetas-backend/controllers/receta.js
+++ recetas-backend/controllers/receta.js
@@ -5,66 +5,54 @@ const { query } = require('express');
 const fs = require('fs');
 require('dotenv').config();
 
-
 receta = {}
 
 receta.reg = async (req, res)=>{
-    //Guarda la fecha del servidor en una variable llamada fecha
+    let Usuario, Receta;
     const Fecha = new Date();
 
-    try{
-        if(!req.body.Token) throw {Mensaje: "No se recibió el token.", Status: 0};
-
-        // Verifica el Token y extrae el id para guardarlo en una variable llamada user
-        const Token = req.body.Token;
+    const validatoken = async function (Token){
         const decoded = jwt.verify(Token, process.env.SECRET);
         const user = decoded.id;
-
-        
-        
-    
-        //Guarda los datos en la tabla llamada Receta y guarda el id de la receta en idR
-        //const queryR = util.promisify(conn.conf.query).bind(conn.conf);
-        //const rowsR = queryR('insert into Receta (Texto, Fecha, Usuario, Costo, Tipo_de_cocina, Lugar, Tiempo, Dificultad, Porciones, Likes) values (?, ?, ?, ?, ?, ?, ?, ?, ?, 0)', [req.body.Texto, Fecha, user, req.body.Costo, req.body.Cocina, req.body.Lugar, req.body.Tiempo, req.body.Dificultad, req.body.Porciones]);
-        
-    
-     
-        
-        /*let path = `./img/${user}/4`;
-        if(!fs.existsSync(path)) {const newdir = await fs.mkdirSync(path, {recursive: true});}
-        const image = req.files.Imagen;
-        path = `${path}/${image.name}`;
-        const moved = await image.mv(path);
-
-        const queryI = util.promisify(conn.conf.query).bind(conn.conf);
-        const rowsI = queryI('insert into Receta_Imagen (Imagen, )');*/
-        conn.conf.query('insert into Receta (Texto, Fecha, Usuario, Costo, Tipo_de_cocina, Lugar, Tiempo, Dificultad, Porciones, Likes) values (?, ?, ?, ?, ?, ?, ?, ?, ?, 0)', [req.body.Texto, Fecha, user, req.body.Costo, req.body.Cocina, req.body.Lugar, req.body.Tiempo, req.body.Dificultad, req.body.Porciones], function async (error, results, fields){
-            if (error) throw error;
-
-            const rID = results.insertId;
-            const path = `./img/${user}/${rID}`;
-
-            if(!fs.existsSync(path)) {
-                fs.mkdirSync(path, {recursive: true}, (dir)=>{
-                    const image = req.files.Imagen;
-                    image.mv(path);
-                });
-            }
-        });
-    }catch(error){
-        res.send({Mensaje: "No se pudo registrar la receta", Error: error}).status(400);
+        return user
     }
 
-    
+    const registrareceta = async function (Texto, Usuario, Costo, Cocina, Lugar, Tiempo, Dificultad, Porciones){
+        try{
+        const queryR = util.promisify(conn.conf.query).bind(conn.conf);
+        const rowsR = queryR('insert into Receta (Texto, Fecha, Usuario, Costo, Tipo_de_cocina, Lugar, Tiempo, Dificultad, Porciones, Likes) values (?, ?, ?, ?, ?, ?, ?, ?, ?, 0)', [Texto, Fecha, Usuario, Costo, Cocina, Lugar, Tiempo, Dificultad, Porciones]);
+        return rowsR;
+        }catch(error){
+            res.send({Mensaje: 'No se pudo registrar la receta', Error: error}).status(400);
+        }
+    }
 
-    res.send({Mensaje: 'Receta registrada con éxito.'}).status(200);
-    
+    const creacarpeta = async function (Receta, Usuario, Imagen){
+        let path = `./img/${Usuario}/${Receta}`;
+        if(!fs.existsSync(path)) {const newdir = await fs.mkdirSync(path, {recursive: true});}
+        //path = `${path}/${Imagen.name}`;
+        return path;
+    }
+
+    const guardarimagen = async function (path){
+        const moved = await image.mv(path);
+        return moved;
+    }
+
+    validatoken(req.body.Token).then((user) => {
+        console.log(user);
+        registrareceta(req.body.Texto, user, req.body.Costo, req.body.Cocina, req.body.Lugar, req.body.Tiempo, req.body.Dificultad, req.body.Porciones).then((rowsR)=>{
+            console.log(rowsR.insertId);
+            creacarpeta(rowsR.insertId, user, req.files.Imagen).then((path) =>{
+                console.log(path);
+                guardarimagen(path).then((moved) =>{
+                    console.log(moved);
+                })
+            })
+        })
+    });
+
+    res.send({Mensaje: 'Receta registrada con éxito.'}).status(200); 
 }
 
-
-
 module.exports = receta; 
-
-//fs.writeFile(newPath, data, function (err) {
-//const dir = `./img/1/1`;
-//if(!fs.existsSync(dir)) { const newdir = await fs.mkdirSync(dir, {recursive: true});}
