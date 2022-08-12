4ef54eba8bf64259afebbf16230077a9e0b7c7b3
Christian Ramos <chrisramfon@outlook.com>
2022-08-12 14:50:31 -0500

    Agrega vistas a bdd, correcciones en registrar receta

--- recetas-backend/controllers/receta.js
+++ recetas-backend/controllers/receta.js
@@ -18,14 +18,11 @@ receta.reg = async (req, res)=>{
         return user
     }
 
-    const registrareceta = async function (Titulo, Texto, Usuario, Costo, Cocina, Lugar, Tiempo, Dificultad, Porciones){
-        try{
+    const registrareceta = async function (Titulo, Texto, Usuario, Costo, Cocina, Lugar, Tiempo, Dificultad, Porciones, Vistas){
+        
         const queryR = util.promisify(conn.conf.query).bind(conn.conf);
-        const rowsR = queryR('insert into Receta (Titulo,Texto, Fecha, Usuario, Costo, Tipo_de_cocina, Lugar, Tiempo, Dificultad, Porciones, Likes) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)', [Titulo, Texto, Fecha, Usuario, Costo, Cocina, Lugar, Tiempo, Dificultad, Porciones]);
-        return rowsR;
-        }catch(error){
-            res.send({Mensaje: 'No se pudo registrar la receta', Error: error}).status(400);
-        }
+        const rowsR = queryR('insert into Receta (Titulo,Texto, Fecha, Usuario, Costo, Tipo_de_cocina, Lugar, Tiempo, Dificultad, Porciones, Likes, Vistas) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0, 0)', [Titulo, Texto, Fecha, Usuario, Costo, Cocina, Lugar, Tiempo, Dificultad, Porciones]);
+        return rowsR;        
     }
 
     const creacarpeta = async function (Receta, Usuario){
@@ -50,10 +47,8 @@ receta.reg = async (req, res)=>{
     .then((Usuario) => registrareceta(req.body.Titulo, req.body.Texto, Usuario, req.body.Costo, req.body.Cocina, req.body.Lugar, req.body.Tiempo, req.body.Dificultad, req.body.Porciones))
     .then((rowsR => creacarpeta(rowsR.insertId, Usuario)))
     .then((path) => guardarimagen(path))
-    .then((moved) =>res.send(moved).status(200)) //mover imagen cuando funcione el front
-    
+    .then((moved) =>res.send({Mensaje: 'Receta registrada', res: moved}).status(200)) //mover imagen cuando funcione el front
 
-    res.send({Mensaje: 'Receta registrada con éxito.'}).status(200); 
 }
 
 receta.explore = async (req, res)=>{
